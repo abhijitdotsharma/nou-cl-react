5d6ac7fdc18d95b9b130c3fcba0811193dafa2f6
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2023-02-05 15:29:13 +0530

    terser and peerDepsExternel added. react, react-dom added as peerDeps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
 
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 import postcss from "rollup-plugin-postcss";
 
@@ -23,12 +25,14 @@ export default [
         },
       ],
       plugins: [
+        peerDepsExternal(),
         resolve(),
         commonjs(),
         typescript({ tsconfig: "./tsconfig.json" }),
         postcss({
             plugins: []
-          })
+          }),
+          terser(),
       ],
     },
     {
